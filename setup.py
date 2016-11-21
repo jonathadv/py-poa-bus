767cb5a38b79c9cf37bcda17c6f6b979ab648d90
Jonatha Daguerre <jonatha@daguerre.com.br>
2016-11-20 23:51:08 -0200

    Adding requirements to setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,21 @@
 # -*- coding: utf-8 -*-
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
 
+# pylint: disable=invalid-name
+
 """
 Setup Script
 """
 
 from distutils.core import setup
 from pypoabus import __version__, __author__, __author_email__, __license__
+from pip.req import parse_requirements
+
+
+install_reqs = parse_requirements('requirements.txt')
+reqs = ['%s (%s%s)' % (ir.req.project_name,
+                       ir.req.specs[0][0],
+                       ir.req.specs[0][1]) for ir in install_reqs]
 
 setup(name='PyPoABus',
       version=__version__,
@@ -17,4 +26,5 @@ setup(name='PyPoABus',
       license=__license__,
       packages=['pypoabus'],
       package_data={'pypoabus': ['config.json']},
+      requires=reqs,
      )
