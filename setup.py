a5d4cb688b75968f824d620786e84efc910f1d6d
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2018-05-25 00:22:43 -0300

    Updates setup.py

--- setup.py
+++ setup.py
@@ -10,13 +10,15 @@ Setup Script
 """
 
 from distutils.core import setup
-from pypoabus import __version__, __author__, __author_email__, __license__
+from pypoabus import __name__, __version__, __author__, __author_email__, __license__
 
-NAME = 'py-poa-bus'
-DESCRIPTION = 'Application to retrieve the bus schedules of Porto Alegre city.'
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+DESCRIPTION = 'Module/CLI to retrieve the bus timetables of Porto Alegre city from EPTC web site.'
 REQUIRED = ['requests', 'beautifulsoup4']
 
-setup(name=NAME,
+setup(name=__name__,
       version=__version__,
       description=DESCRIPTION,
       author=__author__,
@@ -25,4 +27,20 @@ setup(name=NAME,
       packages=['pypoabus'],
       package_data={'pypoabus': ['config.json']},
       requires=REQUIRED,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'Intended Audience :: End Users/Desktop',
+            'Natural Language :: English',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: Implementation :: CPython',
+            'Topic:: Utilities'
+      ]
      )
