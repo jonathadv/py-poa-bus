58e2ad7d899e7952f6f927f0c5df17504bf2ebd9
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2018-05-25 01:12:46 -0300

    Updates module version

--- pypoabus/__init__.py
+++ pypoabus/__init__.py
@@ -7,10 +7,10 @@ PyPoaBus
 Module to connect to EPTC web site and to retrieve the bus lines' data and timetables.
 """
 
-__name__ = 'pypoapus'
+__name__ = 'pypoabus'
 __author__ = 'Jonatha Daguerre'
 __author_email__ = 'jonatha@daguerre.com.br'
 __license__ = 'MIT'
-__version__ = '1.1'
+__version__ = '1.1.0'
 __copyright__ = 'Copyright 2016 Jonatha Daguerre Vasconcelos'
 __all__ = ['eptc_facade', 'exceptions', 'entities']
