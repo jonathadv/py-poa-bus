68a464612bafa1df1670373389af19ccaa9b91b2
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2019-12-11 00:07:19 -0300

    Updates version to 1.3.post1

--- pypoabus/__init__.py
+++ pypoabus/__init__.py
@@ -11,6 +11,6 @@ __title__ = 'pypoabus'
 __author__ = 'Jonatha Daguerre'
 __author_email__ = 'jonatha@daguerre.com.br'
 __license__ = 'MIT'
-__version__ = '1.3.0'
+__version__ = '1.3.post1'
 __copyright__ = 'Copyright 2016 Jonatha Daguerre Vasconcelos'
 __all__ = ['pypoabus', 'exceptions', 'entities']
